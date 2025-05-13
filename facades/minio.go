4989ab7b1eac8ee323b6db40d2e47400708c1cd3
ALMAS <almas1992@126.com>
2025-05-13 11:19:26 +0800

    feat: [#597] add support for package install/uninstall via artisan commands (#81)
    Co-authored-by: maimaiti <maimaiti@archeros.com>

--- facades/minio.go
+++ facades/minio.go
@@ -1,19 +1,16 @@
 package facades
 
 import (
-	"log"
-
 	"github.com/goravel/framework/contracts/filesystem"
 
 	"github.com/goravel/minio"
 )
 
-func Minio(disk string) filesystem.Driver {
+func Minio(disk string) (filesystem.Driver, error) {
 	instance, err := minio.App.MakeWith(minio.Binding, map[string]any{"disk": disk})
 	if err != nil {
-		log.Fatalln(err)
-		return nil
+		return nil, err
 	}
 
-	return instance.(*minio.Minio)
+	return instance.(*minio.Minio), nil
 }
